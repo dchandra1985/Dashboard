2d45cc16e86bcbef8e9dd9cbebf8e2a0aaaacddb
dchandra1985 <chandrasekarandinesh@yahoo.com>
2019-12-10 02:24:11 +0100

    Add existing file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -24,12 +23,14 @@ data = {i for i in range(min(year), max(year))}
 # tmp = df[df['Geography'] == 'Asia']
 
 
-
 # Initialize the application
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
+colors = {
+    'background': '#E6E7E8',
+    'text': '#7FDBFF'
+}
 
 app.layout = html.Div(id='app-content', children=[
 
@@ -40,20 +41,19 @@ app.layout = html.Div(id='app-content', children=[
             html.Div(className='banner', children=[
 
                 html.Img(id="Left_Header_Image",
-                         src="/assets/left.jpg",
-                         style={'float': 'left'}),
+                         src="/assets/left.jpeg",
+                         style={'float': 'left',
+                                'margin-left': '0'}),
 
                 html.Img(id="Right_Header_Image",
-                         src="/assets/right.jpg",
-                         style={'float': 'right'}),
+                         src="/assets/right.jpeg",
+                         style={'float': 'right',
+                                'margin-right': '0'}),
 
-                html.Div(children="Global Emissions Analysis Dashboard",
-                         className='six columns offset-by-two',
-                         style={"textAlign": 'center', 'font-size': '2em', 'color': 'white'}),
-
-                # html.Div(children="Import and of Petroleum",
-                #     className='six columns offset-by-two',
-                #     style={"textAlign": 'center', 'font-size': '1em', 'color' : 'white'}),
+                html.H1(children="Global Emissions Analysis Dashboard",
+                        style={'textAlign': 'center',
+                               'padding': '1.5%',
+                               'font-size': 'calc(1em + 1vw)'}),
 
             ]),
 
@@ -71,6 +71,8 @@ app.layout = html.Div(id='app-content', children=[
             )
         ], className='eight columns'),
 
+        html.Br(),
+
         html.Div(id='interactive', children=[
 
             # Dropdown
@@ -80,17 +82,37 @@ app.layout = html.Div(id='app-content', children=[
                 options=[{'label': i, 'value': i} for i in conty
                          ],
                 searchable=False,
-                placeholder="Select a Continent for Analysis"
+                placeholder="Select Continents for Analysis",
+                multi=True
             ),
 
         ], className='four columns'),
 
-        html.Br(),
-        html.Br(),
+        # Range Slider
+
+        html.Div([
+
+            html.Br(),
+
+            html.Label("Select number of years"),
+
+            dcc.RangeSlider(
+                id="Get_Year",
+                min=min(year),
+                max=max(year),
+                step=5,
+                value=[min(year), max(year)],
+                marks={i: i for i in range(min(year), max(year) + 1, 5)},
+                allowCross=False
+            ),
+
+        ], className='four columns'),
 
         # Dropdown
 
         html.Div([
+
+            html.Br(),
 
             html.Label("X label"),
 
@@ -104,13 +126,11 @@ app.layout = html.Div(id='app-content', children=[
 
         ], className='four columns'),
 
-        html.Br(),
-        html.Br(),
-        html.Br(),
-
         # Dropdown
 
         html.Div([
+
+            html.Br(),
 
             html.Label("y label"),
 
@@ -124,7 +144,6 @@ app.layout = html.Div(id='app-content', children=[
 
             html.Br(),
 
-
             html.Button('Submit',
                         id="Submit-Button",
                         n_clicks=0,
@@ -135,11 +154,12 @@ app.layout = html.Div(id='app-content', children=[
         ], className='four columns'),
 
     ], className="row"),
-    
-        html.Label("© 2019-2020 Dinesh Chandrasekaran. All Rights Reserved.",
-               style={"position": "absolute",
-                "bottom": "0",
-                "left": "0"})
+
+    html.Div(children="© 2019-2020 Dinesh Chandrasekaran. All Rights Reserved.",
+             style={"position": "relative",
+                    "bottom": "-10px",
+                    "left": "8px",
+                    'font-size': 'calc(0.5em + 0.25vw)'})
 
 ])
 
@@ -149,32 +169,43 @@ app.layout = html.Div(id='app-content', children=[
     [Input('Submit-Button', 'n_clicks')],
     [State('Get_Xaxis', 'value'),
      State('Get_yaxis', 'value'),
-     State('Get_Continent', 'value')]
+     State('Get_Continent', 'value'),
+     State('Get_Year', 'value')
+     ]
 )
-def update_graph(n_clicks, X, y, cont):
-
+def update_graph(n_clicks, X, y, cont, year_range):
     if n_clicks is None:  # users would rather write `if n_clicks == 0`
         return ''
     else:
 
-        df1 = df[df['Geography'] == cont]
+        filtered_df = df[(df['Year'] >= year_range[0]) & (df['Year'] <= year_range[1])]
+
+        traces = []
+        for i in cont:
+            df_by_continent = filtered_df[filtered_df['Geography'] == i]
+            traces.append(dict(
+                x=df_by_continent[X],
+                y=df_by_continent[y],
+                mode='markers',
+                opacity=0.7,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'white'}
+                },
+                name=i
+            ))
 
         return {
-            'data': [
-                {'x': df1[X],
-                 'y': df1[y],
-                 'name': '{} and {}'.format(X,y),
-                 'mode': 'markers',
-                 'marker': {'size': 12},
-                 },
-            ],
-            'layout': {
-                'title' : 'Petroleum {} and {} Analysis in {} (Barrel)'.format(X,y,cont),
-                'xaxis' : {'title' : X},
-                'yaxis': {'title': y},
-                'showlegend':True,
-                'hovermode' : 'closest'
-            }
+            'data': traces,
+            'layout': dict(
+                title='{} and {} (barrels per day)'.format(X, y),
+                xaxis={'title': X},
+                yaxis={'title': y},
+                plot_bgcolor=colors['background'],
+                showlegend=True,
+                # 'paper_bgcolor': colors['background'],
+                hovermode='closest'
+            )
         }
 
 
